61f9d2d002b40d117b4328f6103de31f9aea1b32
simsam8 <simvedaa@gmail.com>
2024-09-04 15:48:23 +0200

    implement algorithm

--- algorithms/interval_scheduler.py
+++ algorithms/interval_scheduler.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 """
 Interval Scheduler
 
@@ -8,4 +10,50 @@ Output:
 A maximum size set of pairwise non-overlapping intervals
 
 Solution: Greedy algorithm
+
+
+Algorithm:
+Let R be the input intervals
+    Sort R by finish time
+    Let A = {}
+    while R is not empty:
+        pick the first I in R
+        add I to A
+        remove conflicting intervals from R
+
+    return A
 """
+
+
+def interval_scheduler(intervals: list[tuple]):
+    intervals.sort(key=lambda x: x[1])
+    A = [intervals[0]]
+
+    for interval in intervals:
+        if interval[0] >= A[-1][1]:
+            A.append(interval)
+
+    return A
+
+
+def generate_intervals(n_intervals, max_range):
+    intervals = []
+    for _ in range(n_intervals):
+        start = randrange(0, max_range)
+        end = randrange(start + 1, max_range + 1)
+        intervals.append((start, end))
+
+    return intervals
+
+
+def main():
+    intervals = generate_intervals(5, 10)
+    print("Input Intervals")
+    print(intervals)
+    solution = interval_scheduler(intervals)
+    print("Solution")
+    print(solution)
+
+
+if __name__ == "__main__":
+    main()
