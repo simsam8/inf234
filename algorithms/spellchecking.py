9f6dc279f340ed2642ed41300da685a124dad799
simsam8 <simvedaa@gmail.com>
2024-10-09 15:28:10 +0200

    include dynamic programming version

--- algorithms/spellchecking.py
+++ algorithms/spellchecking.py
@@ -9,16 +9,37 @@ def hamming_distance(word1, word2):
     return distance
 
 
-def leveshtein_distance(word1, word2):
+def levenshtein_distance_recursive(word1, word2):
     if len(word1) == 0:
         return len(word2)
     elif len(word2) == 0:
         return len(word1)
     elif word1[0] == word2[0]:
-        return leveshtein_distance(word1[1:], word2[1:])
+        return levenshtein_distance_recursive(word1[1:], word2[1:])
     else:
         return 1 + min(
-            leveshtein_distance(word1[1:], word2),  # delete from word1
-            leveshtein_distance(word1, word2[1:]),  # insert into word1
-            leveshtein_distance(word1[1:], word2[1:]),  # replace character in word1
+            levenshtein_distance_recursive(word1[1:], word2),  # delete from word1
+            levenshtein_distance_recursive(word1, word2[1:]),  # insert into word1
+            levenshtein_distance_recursive(
+                word1[1:], word2[1:]
+            ),  # replace character in word1
         )
+
+
+def levenshtein_distance_dp(X, Y):
+    OPT = {}
+    lx = len(X)
+    ly = len(Y)
+
+    for i in range(lx + 1):
+        for j in range(ly + 1):
+            if i == 0 or j == 0:
+                OPT[(i, j)] = max(i, j)
+            elif X[i - 1] == Y[j - 1]:
+                OPT[(i, j)] = OPT[(i - 1, j - 1)]
+            else:
+                OPT[(i, j)] = 1 + min(
+                    OPT[(i - 1, j)], OPT[(i, j - 1)], OPT[(i - 1, j - 1)]
+                )
+
+    return OPT[(lx, ly)]
